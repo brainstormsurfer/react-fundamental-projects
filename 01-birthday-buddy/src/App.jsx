14bb40d4e90e774b4a2b1aded61203683558d433
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-15 03:23:29 +0300

    Adding Display All Button - Complete v.03 (final)

--- 01-birthday-buddy/src/App.jsx
+++ 01-birthday-buddy/src/App.jsx
@@ -4,21 +4,27 @@ import List from "./List.jsx";
 const App = () => {  
 const [people, setPeople] = useState(data);
 
+  
 const removePerson = (id) => {
-  const filteredPeople = people.filter((person) => id !== person.id)
-  setPeople(filteredPeople)
+  setPeople(people.filter((person) => id !== person.id))
 }
 
 const clearPeople = () => {
     setPeople([])   
   }
-  
+
+const displayPeople = () => {
+    setPeople(data)   
+  }
+
+
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people}  removePerson={removePerson}/>
       <button className="btn btn-block" onClick={() => clearPeople()}>Clear All</button>
+      <button className="btn btn-block" onClick={() => displayPeople()}>Display All</button>
       </section>
     </main>
   );
