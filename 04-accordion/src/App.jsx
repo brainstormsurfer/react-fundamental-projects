a19642f0899e8a0c97c0c8988264cb9d887e89e9
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-18 01:56:23 +0300

    Adding an Only-one-answer-displayed-at-a-given-time functionality

--- 04-accordion/src/App.jsx
+++ 04-accordion/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import data from "./data";
-import SingleQuestion from './components/SingleQuestion';
+import SingleQuestion from "./components/SingleQuestion";
 
 const App = () => {
   const [questions, setQuestions] = useState([]);
@@ -19,14 +19,25 @@ const App = () => {
   }, []);
 
   const showAnswer = (id) => {
-    const updatedQuestions = questions.map((question) => {
-      if (question.id === id) {
+    const updatedQuestions = questions.map((question, index) => {
+      if (question.id === index + 1 && question.isShow === true) {
         return {
           ...question,
-          isShow: !question.isShow,
+          isShow: false,
+        };
+      } else {
+        if (question.id === id) {
+          return {
+            ...question,
+            isShow: true,
+          };
+        } else {
+          return {
+            ...question,
+            isShow: false,
+          };
         }
       }
-      return question
     });
 
     if (updatedQuestions) {
@@ -38,11 +49,15 @@ const App = () => {
     <main>
       <div className="container">
         <h1>Questions</h1>
-        {
-          questions.map((question) => {
-          return <SingleQuestion key={question.id} {...question} showAnswer={showAnswer}/>
-         })
-      }
+        {questions.map((question) => {
+          return (
+            <SingleQuestion
+              key={question.id}
+              {...question}
+              showAnswer={showAnswer}
+            />
+          );
+        })}
       </div>
     </main>
   );
