b0dd283afc3c2ec3ea03bcf72c5b017c861fc494
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-18 00:42:44 +0300

    Project 04-Accordion visual setup

--- 04-accordion/src/App.jsx
+++ 04-accordion/src/App.jsx
@@ -1,4 +1,42 @@
+import { useState, useEffect } from "react";
+import data from "./data";
+
 const App = () => {
-  return <h2>Accordion Starter</h2>;
+  const [questions, setQuestions] = useState([]);
+  const [isShow, setIsShow] = useState(false)
+
+  useEffect(() => {
+    if (data)  {
+      const questions = data.map((question) => {
+        return {
+          ...question,
+          isShow: false
+        }
+      })
+    }  
+    setQuestions(data);
+  }, []);
+
+  return (
+    <main>
+      <div className="container">
+        <h1>Questions</h1>
+        {questions.map((question) => {
+          return (
+            <div className="question" key={question.id}>
+              <header>
+              <h5>{question.title}</h5>
+              <button className="question-btn">{isShow ? "-" : "+"}</button>
+              </header>
+              {
+                isShow && <p>{question.info}</p>
+              }
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
 };
+
 export default App;
