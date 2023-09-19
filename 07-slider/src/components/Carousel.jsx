bfa5e04b41091f5bb69a1f91b7bef33823e94c49
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-19 13:25:33 +0300

    slide carousel code along

--- 07-slider/src/components/Carousel.jsx
+++ 07-slider/src/components/Carousel.jsx
@@ -1,32 +1,64 @@
-import { useState } from 'react';
-import { shortList, list, longList } from '../data';
-import { FaQuoteRight } from 'react-icons/fa';
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { useState, useEffect } from "react";
+import { shortList, list, longList } from "../data";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
   const [people, setPeople] = useState(longList);
+  const [currentPerson, setCurrentPerson] = useState(0);
 
-  const prevSlide = () => {};
-  const nextSlide = () => {};
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+    const result = (oldPerson - 1 + people.length) % people.length;
+    console.log(result)
+    return result;
+    })
+  };
+
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+    const result = (oldPerson + 1) % people.length;
+    console.log(result)
+    return result;})
+  };
+  
+  useEffect(() => {
+    let sliderId = setInterval(() => {
+        nextSlide();
+    }, 2000);
+
+    return () => {
+        clearInterval(sliderId)
+    }
+  }, [currentPerson]);
+
 
   return (
-    <section className='slider-container'>
-      {people.map((person) => {
+    <section className="slider-container">
+      {people.map((person, personIndex) => {
         const { id, image, title, name, quote } = person;
         return (
-          <article className='slide' key={id}>
-            <img src={image} alt={name} className='person-img' />
-            <h5 className='name'>{name}</h5>
-            <p className='title'>{title}</p>
-            <p className='text'>{quote}</p>
-            <FaQuoteRight className='icon' />
+          <article
+            className="slide"
+            key={id}
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? '1' : '0',
+              visibility: personIndex === currentPerson ? 'visible' : 'hidden'
+            }}
+          >
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
           </article>
         );
       })}
-      <button type='button' className='prev'>
+      <button type="button" className="prev" onClick={() => prevSlide(currentPerson)}>
         <FiChevronLeft />
       </button>
-      <button type='button' className='next'>
+      <button type="button" className="next" onClick={() => nextSlide(currentPerson)}>
         <FiChevronRight />
       </button>
     </section>
