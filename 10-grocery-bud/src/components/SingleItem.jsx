daed5b20487932169124cf3df41e32ebbe25f505
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-23 23:38:06 +0300

    p_style: capitalized first letter (textTransform)

--- 10-grocery-bud/src/components/SingleItem.jsx
+++ 10-grocery-bud/src/components/SingleItem.jsx
@@ -8,8 +8,8 @@ const SingleItem = ({name, completed, removeItem}) => {
     <div className="single-item">
           <input type="checkbox"                     
           checked={isChecked} onChange={() => setIsChecked(!isChecked)}/>
-          <p style={{textDecoration: 
-            isChecked ? "line-through" :"none" }}>{name}</p>
+          <p style={{ textTransform:"capitalize",
+            textDecoration: isChecked ? "line-through" :"none" }}>{name}</p>
           <button type="button" className="btn remove-btn" onClick={removeItem}>Delete</button>
     </div>
   )
