b2d6000b00e65339acd1376571a8b98849c692d8
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-25 04:20:34 +0300

    Completed (along John)

--- 11-navbar/src/components/Navbar.jsx
+++ 11-navbar/src/components/Navbar.jsx
@@ -6,16 +6,18 @@ import logo from "../assets/logo.svg";
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
 
-  const linksContainerRef = useRef(null)
-  const linksRef = useRef(null)
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
-  const toggleLinks = () => {    
-    setShowLinks(!showLinks)
-  }
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
 
   const linkStyles = {
-    height: showLinks ? `${linksRef.current.getBoundingClientRect().height}px` : '0px'
-  }
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
 
   return (
     <nav>
@@ -25,19 +27,34 @@ const Navbar = () => {
           <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
-        </div>       
-          <div className="links-container" ref={linksContainerRef} style={linkStyles}>
-            <ul className="links" ref={linksRef}>
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>        
+        </div>
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyles}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        {/* social links */}
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
