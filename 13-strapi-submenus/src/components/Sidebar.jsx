8e755a42c30d5b1418df7f73b935d07c0972f7e4
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-28 12:07:25 +0300

    Navbar-Hero (jsx,css); Sidebar (jsx)

--- 13-strapi-submenus/src/components/Sidebar.jsx
+++ 13-strapi-submenus/src/components/Sidebar.jsx
@@ -1,9 +1,35 @@
-
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+import sublinks from "./../data";
+import { nanoid } from "nanoid";
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
-    <div>Sidebar</div>
-  )
-}
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <div className="sub-links">
+        {sublinks.map((item) => {
+       const {links, page, pageId} = item
+          return <article key={pageId}>
+            <h4>{page}</h4>
+            <div className="sidebar-sublinks">
+              {links.map((link) => {
+                const {url, icon, label, id} = link
+                return <a key={id} href={url}>
+                  {icon} {label}
+                </a>
+              })}
+            </div>
+          </article>
+        })}
+      </div>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
