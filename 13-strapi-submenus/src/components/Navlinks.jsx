8c1b2e28d4e89639467d9100fae6fa5f3424fc73
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-28 17:37:38 +0300

    Submenu functionality

--- 13-strapi-submenus/src/components/Navlinks.jsx
+++ 13-strapi-submenus/src/components/Navlinks.jsx
@@ -1,15 +1,18 @@
 import { useGlobalContext } from "./Context";
-
 import sublinks from "../data";
 
 const NavLinks = () => {
-  return (
-    <div className="nav-links">
+    const {setPageId} = useGlobalContext()
+
+    return (
+        <div className="nav-links">
       (
       {sublinks.map((item) => {
-        const { pageId, page } = item;
-        return (
-          <button key={pageId} className="nav-link">
+          const { pageId, page } = item;
+          return (
+              <button key={pageId} className="nav-link"
+              onMouseOver={() => setPageId(pageId)}
+              >
             {page}
           </button>
         );
--- 13-strapi-submenus/src/components/Submenu.jsx
+++ 13-strapi-submenus/src/components/Submenu.jsx
@@ -1,8 +1,30 @@
+import sublinks from "../data";
+import { useGlobalContext } from "./Context";
 
 const Submenu = () => {
+  const { pageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
   return (
-    <div>Submenu</div>
-  )
-}
+    <div className="submenu">
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon} {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Submenu
+export default Submenu;
