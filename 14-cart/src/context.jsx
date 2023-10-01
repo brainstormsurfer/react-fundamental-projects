d0a962c5af3b34e689a0263437992cc9e10aaa0f
brainstromsurfer <wishmeheaven@gmail.com>
2023-10-01 18:03:26 +0300

    clear cart (code along), remove item (self exercise)

--- 14-cart/src/context.jsx
+++ 14-cart/src/context.jsx
@@ -2,26 +2,38 @@ import { createContext, useContext, useEffect, useReducer } from "react";
 import  reducer from './reducer';
 import {
     CLEAR_CART,
-    REMOVE,
+    REMOVE_ITEM,
     INCREASE,
     DECREASE,
     LOADING,
     DISPLAY_ITEMS,
   } from "./ACTIONS";
-  
-const AppContext = createContext();
+import cartItems from "./data";
 
+
+const AppContext = createContext();
 
 const initialState = {
   loading: false,
-  cart: [],
+  cart: new Map(cartItems.map((item) => [item.id, item])),
 };
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART })
+  }
+
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: {id}})
+  }
+
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider
+     value={{ ...state, clearCart, removeItem }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
--- 14-cart/src/reducer.jsx
+++ 14-cart/src/reducer.jsx
@@ -1,14 +1,39 @@
 import {
-    CLEAR_CART,
-    REMOVE,
-    INCREASE,
-    DECREASE,
-    LOADING,
-    DISPLAY_ITEMS,
-  } from "./ACTIONS";
-  
-const reducer = (state, action) => {
-    return state;
-  };
+  CLEAR_CART,
+  REMOVE_ITEM,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./ACTIONS";
+import cartItems from "./data";
+// console.log('cartItems' , cartItems)
 
-  export default reducer
+const reducer = (state, action) => {  
+// console.log('state.cart', (state.cart))
+// console.log('Array.from(state.cart)', Array.from(state.cart.entries()))
+  const {payload} = action
+  switch (action.type) {
+    case CLEAR_CART: {
+      return { ...state, cart: new Map() };
+    }
+    case REMOVE_ITEM: { 
+        const updateCart = Array.from(state.cart.entries())
+        console.log('updateCart',updateCart)
+        console.log('updateCart[0][1]',updateCart[0][1])
+        // ? why item[0] is not return id ?
+        const items = updateCart.map((item) => item[1])
+        console.log('items',items)
+        const filteredCart = items.filter((item) => item.id !== payload.id)
+        console.log('filteredCart',filteredCart)
+        const cartMap = new Map(filteredCart.map((item) => [item.id, item]))
+        console.log('cartMap', cartMap)
+        return {...state, cart: cartMap}
+    }
+    default: {
+      throw new Error(`no matching action type : ${action.type}`);
+    }
+  }
+};
+
+export default reducer;
