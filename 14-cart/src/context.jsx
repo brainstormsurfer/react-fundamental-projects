d4ea4bcc7c1d9652ab7c5754026567adeb56feb8
brainstromsurfer <wishmeheaven@gmail.com>
2023-10-01 03:12:24 +0300

    Cart project (#14) - setting up (context, reducer, ACTIONS)

---
+++ 14-cart/src/context.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import  reducer from './reducer';
+import {
+    CLEAR_CART,
+    REMOVE,
+    INCREASE,
+    DECREASE,
+    LOADING,
+    DISPLAY_ITEMS,
+  } from "./ACTIONS";
+  
+const AppContext = createContext();
+
+
+const initialState = {
+  loading: false,
+  cart: [],
+};
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
